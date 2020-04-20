c7843dfcc05534b9431090e261076ad6e94c2a4a
Johan Jordaan <djjordaan@gmail.com>
2020-04-21 09:28:59 +1000

    Fixed order on duplicates

--- Duped/FileAnalyser.cs
+++ Duped/FileAnalyser.cs
@@ -15,13 +15,25 @@ namespace Duped
 
         public class Result
         {
+            public int TotalFileCount { private set;  get; }
+            public int UniqueFileCount { private set; get; }
+
+            public Result(int totalFileCount,int uniqueFileCount)
+            {
+                TotalFileCount = totalFileCount;
+                UniqueFileCount = uniqueFileCount;
+            }
         }
 
         public static FileAnalyser.Result Run(List<Duped.FileInfoWrapper> files)
         {
-            var result = new Result();
+            var uniqueByNameAndSize = files
+                .GroupBy(x => $"{x.Source.Name.ToLower()}__{x.Source.Length}")
+                .ToDictionary(x => x.Key, x => x.OrderBy(x=>x.Depth).ToList());
 
-            var uniqueByNameAndSize = files.GroupBy(x => $"{x.Source.Name.ToLower()}__{x.Source.Length}").ToDictionary(x => x.Key, x => x.ToList());
+
+            var result = new Result(files.Count,uniqueByNameAndSize.Count);
+
 
             return result;
         }
