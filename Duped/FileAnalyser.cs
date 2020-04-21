7c86d9e0caf79dc0130299651409a61cc191bdf7
Johan Jordaan <djjordaan@gmail.com>
2020-04-21 17:19:41 +1000

    Extra tests and the recommendation class

--- Duped/FileAnalyser.cs
+++ Duped/FileAnalyser.cs
@@ -17,24 +17,46 @@ namespace Duped
         {
             public int TotalFileCount { private set;  get; }
             public int UniqueFileCount { private set; get; }
+            public List<Recommendation> Recommendations { private set; get; }
+            public long TimeTakenMs { set; get; }
 
-            public Result(int totalFileCount,int uniqueFileCount)
+            public Result(int totalFileCount,int uniqueFileCount, List<Recommendation> recommendations)
             {
                 TotalFileCount = totalFileCount;
                 UniqueFileCount = uniqueFileCount;
+                Recommendations = recommendations;
+            }
+        }
+
+        public class Recommendation
+        {
+            public FileInfoWrapper Keep { private set;  get; }
+            public List<FileInfoWrapper> Remove { private set; get; }
+
+            public Recommendation(IEnumerable<FileInfoWrapper> matchedFiles)
+            {
+                Remove = matchedFiles.OrderByDescending(x => x.Depth).ToList();
+                Keep = Remove[0];
+                Remove.RemoveAt(0);
             }
         }
 
         public static FileAnalyser.Result Run(List<Duped.FileInfoWrapper> files)
         {
-            var uniqueByNameAndSize = files
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            var recommendations = files
                 .GroupBy(x => $"{x.Source.Name.ToLower()}__{x.Source.Length}")
-                .ToDictionary(x => x.Key, x => x.OrderBy(x=>x.Depth).ToList());
+                .ToDictionary(x => x.Key, x => new Recommendation(x))
+                .Values
+                .ToList();
 
 
-            var result = new Result(files.Count,uniqueByNameAndSize.Count);
+            var result = new Result(files.Count,recommendations.Count,recommendations);
 
-
+            watch.Stop();
+            result.TimeTakenMs = watch.ElapsedMilliseconds;
             return result;
         }
 
--- DupedTests/FileAnalyserTests.cs
+++ DupedTests/FileAnalyserTests.cs
@@ -15,6 +15,13 @@ namespace DupedTests
             var files = Duped.DirWalker.Run("../../../fixtures/src").Files;
             Assert.AreEqual(2, files.Count);
             var result = Duped.FileAnalyser.Run(files);
+            Assert.AreEqual(2, result.TotalFileCount);
+            Assert.AreEqual(1, result.UniqueFileCount);
+            Assert.AreEqual(1, result.Recommendations.Count);
+            Assert.AreEqual("file-one.txt", result.Recommendations[0].Keep.Source.Name);
+            Assert.AreEqual(1, result.Recommendations[0].Remove.Count);
+            Assert.AreEqual("file-one.txt", result.Recommendations[0].Remove[0].Source.Name);
+
 
         }
     }
