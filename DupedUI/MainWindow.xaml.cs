06bba9f33548d054373404f56e33efbeaea0970e
Johan Jordaan <djjordaan@gmail.com>
2020-04-30 13:34:47 +1000

    Some ui

--- DupedUI/MainWindow.xaml.cs
+++ DupedUI/MainWindow.xaml.cs
@@ -22,9 +22,14 @@ namespace DupedUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<String> roots = new List<String>();
+        private List<String> files = new List<String>();
+        private bool _wasCancelled = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            Status.XTitle = "Hallo";
         }
 
 
@@ -33,24 +38,58 @@ namespace DupedUI
             var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
             if (dialog.ShowDialog(this).GetValueOrDefault())
             {
-                SelectedPath.Content = dialog.SelectedPath;
+                roots.Add(dialog.SelectedPath);
+                SelectedPathLbl.Content = dialog.SelectedPath;
             }
 
-            ProcessFiles(dialog.SelectedPath);
+            
         }
 
         private async Task ProcessFiles(string root)
         {
-            var files = new List<string>();
             foreach (var progress in Duped.DirWalker.Files(root))
             {
+                if(_wasCancelled)
+                {
+                    break;
+                }
                 await Dispatcher.Yield(DispatcherPriority.ApplicationIdle);
-                files.Add(progress.FileName);
-                Status.Content = $"{progress.FileName} - {progress.DiretoriesInQueue}";
-                Count.Content = $"{files.Count}";
+                this.files.Add(progress.FileName);
+                StatusLbl.Content = $"{progress.FileName} - {progress.DiretoriesInQueue}";
+                CountLbl.Content = $"{files.Count}";
+                Status.SeriesCollection[0].Values.Clear();
+                Status.SeriesCollection[0].Values.Add(Convert.ToDouble(files.Count));
             }
 
-            Status.Content = "Done ...";
+            StatusLbl.Content = "Done ...";
+            StartBtn.IsEnabled = true;
+            AddTargetBtn.IsEnabled = true;
+            CancelBtn.IsEnabled = false;
+
+        }
+
+        async private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StartBtn.IsEnabled = false;
+            AddTargetBtn.IsEnabled = false;
+            CancelBtn.IsEnabled = true;
+            _wasCancelled = false;
+            this.files.Clear();
+            foreach (String root in roots) {
+                await ProcessFiles(root);
+            }
+            StatusLbl.Content = "Done ...";
+            StartBtn.IsEnabled = true;
+            AddTargetBtn.IsEnabled = true;
+            CancelBtn.IsEnabled = false;
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StartBtn.IsEnabled = true;
+            AddTargetBtn.IsEnabled = true;
+            CancelBtn.IsEnabled = false;
+            _wasCancelled = true;
         }
     }
 }
---
+++ DupedUI/StatusGraph.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace DupedUI
+{
+    /// <summary>
+    /// Interaction logic for StatusGraph.xaml
+    /// </summary>
+    public partial class StatusGraph : UserControl
+    {
+        public StatusGraph()
+        {
+            InitializeComponent();
+
+            SeriesCollection.Add(new RowSeries
+            {
+                Title = "2015",
+                Values = new ChartValues<double> { 10, 50, 39, 50 }
+             
+            });
+
+            //adding series will update and animate the chart automatically
+            SeriesCollection.Add(new RowSeries
+            {
+                Title = "2016",
+                Values = new ChartValues<double> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+
+            DataContext = this;
+        }
+
+        public string XTitle { get; set; } = "Status";
+        public string YTitle { get; set; } = "";
+
+        public SeriesCollection SeriesCollection { get; set; } = new SeriesCollection();
+        public string[] Labels { get; set; } = new string[] { };
+        public string Formatter(string value)
+        {
+            return value.ToString();
+        }
+    }
+}
